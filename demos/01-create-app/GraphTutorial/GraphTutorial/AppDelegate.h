4b965f7e3c5d78a9d0c6be769479b27c02b594be
Jason Johnston <jasonjoh@users.noreply.github.com>
2019-08-20 16:29:26 -0400

    Created single view app in Xcode

---
+++ demos/01-create-app/GraphTutorial/GraphTutorial/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GraphTutorial
+//
+//  Created by Jason Johnston on 8/20/19.
+//  Copyright © 2019 Jason Johnston. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ demos/01-create-app/GraphTutorial/GraphTutorial/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GraphTutorial
+//
+//  Created by Jason Johnston on 8/20/19.
+//  Copyright © 2019 Jason Johnston. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
