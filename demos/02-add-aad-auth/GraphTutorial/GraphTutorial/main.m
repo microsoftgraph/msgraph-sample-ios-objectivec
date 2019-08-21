88d0dfbabd174de2865e292eb9e171e08e458d2d
Jason Johnston <jasonjoh@users.noreply.github.com>
2019-08-21 12:11:42 -0400

    Copied completed 01 project to 02 folder

---
+++ demos/02-add-aad-auth/GraphTutorial/GraphTutorial/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GraphTutorial
+//
+//  Copyright © 2019 Microsoft. All rights reserved.
+//  Licensed under the MIT license. See LICENSE.txt in the project root for license information.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
